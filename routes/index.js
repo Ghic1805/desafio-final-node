6c5d8273d7e458cb42ce1dbbf93755beb389e341
Guilherme Irie <guilherme.irie@wattei.com.br>
2021-06-10 17:26:37 -0300

    usando partials

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,7 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
     let obj = {
-        'nome': 'Gui',
-        'idade': 22,
-        mostrar: true,
-        ingredientes: [
-            {nome: 'Arroz', qt: '20kg'},
-            {nome: 'Sorvete', qt: '200g'}
-        ],
-        interesses: [ 'node', 'js', 'css'],
-        teste: '<strong>testando negrito</strong> '
+        nome: req.query.nome
     };
 
     res.render('home', obj);

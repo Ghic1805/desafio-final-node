6e499e39d7745b7ac0284f86f87e4e9bd7fb030a
Guilherme Irie <guilherme.irie@wattei.com.br>
2021-06-10 17:10:29 -0300

    entendendo template parte 1

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-let obj = {
-    'nome': 'Gui',
-    'idade': 22
-}
+    let obj = {
+        'nome': 'Gui',
+        'idade': 22,
+        mostrar: true,
+        ingredientes: [
+            {nome: 'Arroz', qt: '20kg'},
+            {nome: 'Sorvete', qt: '200g'}
+        ]
+    };
 
     res.render('home', obj);
 });

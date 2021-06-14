2be0e2da3dfc8d36e0e12daeb7cd9d996084ad62
Guilherme Irie <guilherme.irie@wattei.com.br>
2021-06-14 14:22:30 -0300

    criando usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,23 @@
+const User = require('../models/User');
+const user = require('../models/User')
+
 exports.login = (req, res) => {
     res.render('login');
+};
+
+exports.register = (req, res) => {
+    res.render('register');
+};
+
+exports.registerAction = (req, res) => {
+    const newUser = new User(req.body);
+    user.register(newUser, req.body.password, (error)=>{
+        if(error) {
+            console.log('Error ao registrar: ', error);
+            res.redirect('/');
+            return;
+        }
+
+        res.redirect('/');
+    });
 };
--- helpers.js
+++ helpers.js
@@ -3,5 +3,6 @@ exports.defaultPagetitle = "Site ABC";
 exports.menu = [
     {nome: 'Home', slug: '/'},
     {nome: 'Login', slug: '/users/login'},
+    {nome: 'Cadastro', slug: '/users/register'},
     {nome: 'Adicionar Post', slug: '/post/add'}
 ];

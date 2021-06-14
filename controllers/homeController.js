87a2285ad81b0fc74d56b12ead1a3039f8ddd802
Guilherme Irie <guilherme.irie@wattei.com.br>
2021-06-14 13:30:20 -0300

    tags com mongodb aggregation

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,24 @@
+const { response } = require('express');
 const mongoose = require('mongoose');
 const Post = mongoose.model('Post');
 
 exports.index = async (req, res) => {
     let responseJson = {
         pageTitle: 'Titulo de teste',
-        posts: []
+        posts: [],
+        tags: [],
+        tag: ''
     };
+
+    responseJson.tag = req.query.t;
+
+    const tags = await Post.getTagsList();
+    for(let i in tags) {
+        if(tags[i]._id == responseJson.tag) {
+            tags[i].class = "selected";
+        }
+    }
+    responseJson.tags = tags;
 
     const posts = await Post.find();
     responseJson.posts= posts;

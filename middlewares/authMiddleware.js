0a91e046076c57e48b4e949ff2e1471e95d08fee
Guilherme Irie <guilherme.irie@wattei.com.br>
2021-06-14 17:39:58 -0300

    alterar senha

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-module.exports.isLogged = (req, res, next) => {
+exports.isLogged = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.flash('error', 'Ops! Você não tem permissão para acessar esta página.');
         res.redirect('/users/login');
@@ -6,4 +6,21 @@ module.exports.isLogged = (req, res, next) => {
     }
 
     next();
+};
+
+exports.changePassword = (req, res) => {
+    //1- Confirmar se as senhas batem
+    if(req.body.password != req.body['password-confirm']) {
+        req.flash('error', 'Senhanão batem')
+        res.redirect('/profile');
+        return;
+    }
+    //2- Procurar o usuario e trocar a senha dele
+    req.user.setPassword(req.body.password, async ()=> {
+        await req.user.save();
+
+        req.flash('success', 'Senha alterada com sucesso!')
+        res.redirect('/');
+    });
+    //3- Redirecionar para a HOME
 };
